3e283915f20a7511168cb84d2751a8a6b252e07a
averak <abe12@mccc.jp>
2021-09-09 05:18:18 +0900

    [test] todo repository unit test

---
+++ test/abstract_UT.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class Abstract_UT(TestCase):
+    SAMPLE_INT: int = 0
+    SAMPLE_STR: str = 'SAMPLE'
+    SAMPLE_BOOL: bool = False
---
+++ test/test_todo_repository_UT.py
@@ -0,0 +1,84 @@
+from test.abstract_UT import Abstract_UT
+from test.sample.todo_sample import TodoSample
+from repository.todo_repository import TodoRepository
+
+
+class TodoRepository_UT(Abstract_UT):
+    todo_repository = TodoRepository()
+
+    def setUp(self):
+        self.todo_repository.todos = {}
+
+    def test_正_TODOを作成する(self):
+        # setup
+        todo = TodoSample.builder().build()
+
+        # test
+        self.todo_repository.insert(todo)
+
+        # verify
+        created_todos = self.todo_repository.todos
+        self.assertEqual(1, len(created_todos))
+        self.assertEqual(todo, created_todos[todo.id])
+
+    def test_正_TODOを更新する(self):
+        # setup
+        todo = TodoSample.builder().build()
+        self.todo_repository.insert(todo)
+
+        # test
+        todo.title = 'changed title'
+        self.todo_repository.update(todo)
+
+        # verify
+        updated_todo = self.todo_repository.todos[todo.id]
+        self.assertEqual(todo, updated_todo)
+
+    def test_正_TODOを削除する(self):
+        # setup
+        todo = TodoSample.builder().build()
+        self.todo_repository.insert(todo)
+
+        # test
+        self.todo_repository.delete(todo.id)
+
+        # verify
+        self.assertEqual(0, len(self.todo_repository.todos))
+
+    def test_正_IDからTODOを取得(self):
+        # setup
+        todo = TodoSample.builder().build()
+        self.todo_repository.insert(todo)
+
+        # verify
+        self.assertEqual(todo, self.todo_repository.select_by_id(todo.id))
+
+    def test_正_TODOを全件取得する(self):
+        # setup
+        todos = [
+            TodoSample.builder().build(),
+            TodoSample.builder().build(),
+            TodoSample.builder().build(),
+        ]
+        list(map(self.todo_repository.insert, todos))
+
+        # test
+        selected_todos = self.todo_repository.select_all()
+
+        # verify
+        self.assertEqual(selected_todos, todos)
+
+    def test_正_TODOを全て削除(self):
+        # setup
+        todos = [
+            TodoSample.builder().build(),
+            TodoSample.builder().build(),
+            TodoSample.builder().build(),
+        ]
+        list(map(self.todo_repository.insert, todos))
+
+        # test
+        self.todo_repository.delete_all()
+
+        # verify
+        self.assertEqual(0, len(self.todo_repository.todos))
