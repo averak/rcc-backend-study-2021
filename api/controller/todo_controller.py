085c821206ce3baa4167b00c35d8666d78a6f57b
averak <abe12@mccc.jp>
2021-09-09 06:29:52 +0900

    [feat] implement todo crud api

--- api/controller/todo_controller.py
+++ api/controller/todo_controller.py
@@ -2,14 +2,43 @@ from fastapi import APIRouter
 
 from model.todo_model import TodoModel
 from service.todo_service import TodoService
+from api.request.todo_create_request import TodoCreateRequest
+from api.request.todo_update_request import TodoUpdateRequest
 from api.response.todos_response import TodosResponse
 
 router = APIRouter()
 todo_service = TodoService()
 
 
-@router.get("/todos/", status_code=200, response_model=TodosResponse)
+@router.get("/todos", status_code=200, response_model=TodosResponse, description="TODOリストを取得")
 async def get_todos() -> list[TodoModel]:
+    """
+    TODOリストを取得する
+    """
     return TodosResponse(
         todos=todo_service.get_todos()
     )
+
+
+@router.post("/todos", status_code=201, description="TODOを作成")
+async def create_todo(request_body: TodoCreateRequest) -> None:
+    """
+    TODOを作成する
+    """
+    todo_service.create_todo(request_body)
+
+
+@router.put("/todos/{todo_id}", status_code=200, description="TODOを更新")
+async def update_todo(todo_id: int, request_body: TodoUpdateRequest) -> None:
+    """
+    TODOを更新する
+    """
+    todo_service.update_todo(todo_id, request_body)
+
+
+@router.delete("/todos/{todo_id}", status_code=200, description="TODOを削除")
+async def delete_todo(todo_id: int) -> None:
+    """
+    TODOを削除する
+    """
+    todo_service.delete_todo(todo_id)
--- service/todo_service.py
+++ service/todo_service.py
@@ -1,9 +1,41 @@
 from model.todo_model import TodoModel
 from repository.todo_repository import TodoRepository
+from api.request.todo_create_request import TodoCreateRequest
+from api.request.todo_update_request import TodoUpdateRequest
 
 
 class TodoService:
     todo_repository = TodoRepository()
 
     def get_todos(self) -> list[TodoModel]:
+        """
+        TODOリストを取得する
+        """
         return self.todo_repository.select_all()
+
+    def create_todo(self, request_body: TodoCreateRequest) -> None:
+        """
+        TODOを作成する
+        """
+        todo = TodoModel(
+            id=-1,  # IDを仮で指定する
+            title=request_body.title,
+            description=request_body.description,
+            is_done=False,
+        )
+        self.todo_repository.insert(todo)
+
+    def update_todo(self, todo_id: int, request_body: TodoUpdateRequest) -> None:
+        """
+        TODOを更新する
+        """
+        todo = self.todo_repository.select_by_id(todo_id)
+        todo.title = request_body.title
+        todo.description = request_body.description
+        self.todo_repository.update(todo)
+
+    def delete_todo(self, todo_id: int) -> None:
+        """
+        TODOを削除する
+        """
+        self.todo_repository.delete_by_id(todo_id)
